489f66232c0c49d193ac2c4ca59ab5006eb4be4e
Brayan Macias <brayandresmt@outlook.com>
2022-05-10 17:50:05 -0500

    Instantiating AppDatabase

--- app/src/main/java/edu/eci/ieti/taskplannerapp/diModule/DataModule.java
+++ app/src/main/java/edu/eci/ieti/taskplannerapp/diModule/DataModule.java
@@ -1,5 +1,9 @@
 package edu.eci.ieti.taskplannerapp.diModule;
 
+import android.content.Context;
+
+import androidx.room.Room;
+
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import javax.inject.Singleton;
@@ -7,10 +11,13 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
 import dagger.hilt.components.SingletonComponent;
 
 import edu.eci.ieti.taskplannerapp.analytics.AnalyticsAdapter;
 import edu.eci.ieti.taskplannerapp.analytics.FlurryAnalytics;
+import edu.eci.ieti.taskplannerapp.persistence.AppDatabase;
+import edu.eci.ieti.taskplannerapp.persistence.dao.UserDao;
 
 @Module
 @InstallIn( SingletonComponent.class )
@@ -26,5 +33,17 @@ public class DataModule {
     @Singleton
     public Executor provideExecutor() {
         return Executors.newFixedThreadPool(1);
+    }
+
+    @Provides
+    @Singleton
+    public AppDatabase provideAppDataBase(@ApplicationContext Context context){
+        return Room.databaseBuilder(context, AppDatabase.class, "database-name").build();
+    }
+
+    @Provides
+    @Singleton
+    public UserDao provideUserDao(AppDatabase appDatabase){
+        return appDatabase.userDao();
     }
 }
